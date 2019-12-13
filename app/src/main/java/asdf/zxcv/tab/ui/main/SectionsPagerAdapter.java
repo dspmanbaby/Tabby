cb2d06a63a1c89a347f9fe3d31eab2acbab013a7
dspmanbaby <32938970+dspmanbaby@users.noreply.github.com>
2019-12-13 13:27:06 -0800

    Initial commit

---
+++ app/src/main/java/asdf/zxcv/tab/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,59 @@
+package asdf.zxcv.tab.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import asdf.zxcv.tab.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.breakfast, R.string.lunch, R.string.snackies,R.string.dinner};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch(position){
+            case 0:
+                fragment = new BreakFastFragment();
+                break;
+            case 1:
+                fragment = new SnackiesFragment();
+                break;
+            case 2:
+                fragment = new LunchFragment();
+                break;
+            case 3:
+                fragment = new DinnerFragment();
+                break;
+        }
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 4;
+    }
+}
